6848a952ca00c4eb3554b737e29cc1ed6d809d91
anastasisgiann <anastasiosgiannoulidhs@gmail.com>
2025-12-02 23:39:30 +0200

    add projects component, update app component, update css,add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 
+import { useState } from "react";
 import { Courses } from "./Courses";
 import Course from './components/Course';
 import Header from './components/Header';
+import ProjectButton from "./components/ProjectButton";
+import { PROJECTS } from "./projects";
+
 
 function App() {
+  const [selectedProject, setSelectedProject] = useState();
+
+  let buttonContent = <p>Please select a Project!</p>
+
+  if(selectedProject){
+    buttonContent = (
+      <div id="tab-content">
+            <img src={PROJECTS[selectedProject].image}></img>
+            <div><h3>{PROJECTS[selectedProject].title}</h3>
+            <p>{PROJECTS[selectedProject].description}</p>
+            <h3>{PROJECTS[selectedProject].techt}</h3>
+            <p>{PROJECTS[selectedProject].tech}</p>
+            </div>
+          </div>
+    ) 
+  };
+
+  function handleProjects(selectedButton){
+    setSelectedProject(selectedButton)
+  };
   return (
     <div>
       <Header />
@@ -15,6 +39,21 @@ function App() {
             <Course {...Courses[1]}/>
             <Course {...Courses[2]}/>
           </ul>
+        </section>
+        <section id="examples">
+          <h2>PROJECTS</h2>
+          <menu>
+            <ProjectButton 
+            isSelected={selectedProject === 'wedding'}
+            onSelect={()=> handleProjects('wedding')}>Wedding Invitation</ProjectButton>
+            <ProjectButton 
+            isSelected={selectedProject === 'OurGymSKG'}
+            onSelect={()=> handleProjects('OurGymSKG')}>OurGymSKG</ProjectButton>
+            <ProjectButton 
+            isSelected={selectedProject === 'CV'}
+            onSelect={()=> handleProjects('CV')}>My CV</ProjectButton>
+          </menu>
+         {buttonContent} 
         </section>
       </main>
       
---
+++ src/projects.js
@@ -0,0 +1,27 @@
+import weddingGif from './assets/weddinggif.gif';
+import skg from './assets/website.gif';
+
+export const PROJECTS = {
+    wedding: {
+        title:'Wedding Invitation',
+        image: weddingGif,
+        description: 'This project is a digital single-page wedding invitation designed to share key details such as the ceremony time, church location, venue information, photos, and maps. It is optimized primarily for mobile devices but remains fully responsive across larger screens as well.',
+        techt: 'Tech stack',
+        tech: 'HTML , CSS, JavaScript, Bootstrap.'
+    },
+    OurGymSKG: {
+        title:'OurGymSKG',
+        image: skg,
+        description: 'OurGymSKG is a simple, responsive website for a local gym, featuring a Home and Register page. The project was developed as part of a Masters level course exercise.',
+        techt: 'Tech stack',
+        tech: 'HTML , CSS, JavaScript, Bootstrap.'
+    },
+        CV: {
+        title:'Anastasios Giannoulidis CV',
+        image: weddingGif,
+        description: 'This project is a digital single-page wedding invitation designed to share key details such as the ceremony time, church location, venue information, photos, and maps. Built with HTML, CSS, Bootstrap, and JavaScript, it is optimized primarily for mobile devices but remains fully responsive across larger screens as well.',
+        techt: 'Tech stack',
+        tech: 'React , CSS, JavaScript.'
+    }
+}
+    
